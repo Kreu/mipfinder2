7038752d167aa251d655a7ad77b9651f1c3b21d6
Kreu <kruusvee.valdeko@gmail.com>
2019-05-10 19:11:35 +0200

    Add type hints to some variables

--- mipfinder/config.py
+++ mipfinder/config.py
@@ -7,12 +7,12 @@ logging.getLogger(__name__)
 class Config:
   """Test for doc"""
 
-  def __init__(self, config_file):
+  def __init__(self, config_file: str):
     self.config_file = config_file
     self.readConfiguration(config_file)
     self.verifyConfiguration()
 
-  def readConfiguration(self, config_file):
+  def readConfiguration(self):
     """Reads and verifies configuration file for miPFinder
 
     Reads in the user-defined configuration file and then verifies the parameters.
@@ -28,19 +28,19 @@ class Config:
 
     logging.info("Reading configuration file...")
     config = configparser.ConfigParser()
-    config.read(config_file)
+    config.read(self.config_file)
 
     # MIPFINDER configuration section
-    self.protein_list = config['MIPFINDER']['protein_list']
-    self.species_prefix = config['MIPFINDER']['species_prefix']
-    self.maximum_mip_length = int(config['MIPFINDER']['maximum_mip_length'])
-    self.minimum_ancestor_length = int(config['MIPFINDER']['minimum_ancestor_length'])
-    self.blast_cutoff = float(config['MIPFINDER']['blast_cutoff'])
-    self.overlap_cutoff = float(config['MIPFINDER']['overlap_cutoff'])
-    self.e_value_cutoff = float(config['MIPFINDER']['e_value_cutoff'])
-    self.c_value_cutoff = float(config['MIPFINDER']['c_value_cutoff'])
+    self.protein_list : str = config['MIPFINDER']['protein_list']
+    self.species_prefix : str = config['MIPFINDER']['species_prefix']
+    self.maximum_mip_length : int = int(config['MIPFINDER']['maximum_mip_length'])
+    self.minimum_ancestor_length : int = int(config['MIPFINDER']['minimum_ancestor_length'])
+    self.blast_cutoff : float = float(config['MIPFINDER']['blast_cutoff'])
+    self.overlap_cutoff : float = float(config['MIPFINDER']['overlap_cutoff'])
+    self.e_value_cutoff : float = float(config['MIPFINDER']['e_value_cutoff'])
+    self.c_value_cutoff : float = float(config['MIPFINDER']['c_value_cutoff'])
 
-    # TODO: Rename
+    # TODO: Rename these files
     self.mostSIMILARcutoff = config['MIPFINDER']['mostSIMILARcutoff']
     self.open_gap_penalty = config['MIPFINDER']['open_gap_penalty']
     self.maxMIPgroupSIZE = config['MIPFINDER']['maxMIPgroupSIZE']
@@ -54,6 +54,11 @@ class Config:
     self.annotation = config['DATA']['annotation']
     self.known_microproteins = config['DATA']['known_microproteins']
 
+    # PATHS configuration section
+    self.blast_path = config['PATHS']['blast_path']
+    self.clustal_path = config['PATHS']['clustal_path']
+    self.hmmsearch_path = config['PATHS']['hmmsearch_path']
+
     # STRING configuration section
     self.string_database = config['STRING']['STRING_database']
     self.string_column = config['STRING']['STRING_column']
@@ -64,24 +69,24 @@ class Config:
 
     logging.info("Verifying configuration file...")
     if self.maximum_mip_length >= self.minimum_ancestor_length:
-      logging.info(f'In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.')
-      raise ValueError(f'In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.')
+      logging.info(f"In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.")
+      raise ValueError(f"In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.")
 
     if not self.protein_gene_list:
-      logging.info(f'Warning: protein_gene_list parameter is not set in {self.config_file}. Will not consider gene-protein relations.')
+      logging.info(f"Warning: protein_gene_list parameter is not set in {self.config_file}. Will not consider gene-protein relations.")
 
     if not self.annotation:
-      logging.info(f'Warning: annotation parameter is not set in {self.config_file}. Will not add protein annotations.')
+      logging.info(f"Warning: annotation parameter is not set in {self.config_file}. Will not add protein annotations.")
 
     if not self.ipfam_database:
-      logging.info(f'warning: ipfam_database parameter is not set in {self.config_file}. Will not add domain interaction information.')
+      logging.info(f"Warning: ipfam_database parameter is not set in {self.config_file}. Will not add domain interaction information.")
 
     if not self.string_database:
-      logging.info(f'Warning: string_database parameter is not set in {self.config_file}. Will not add interaction information.')
+      logging.info(f"Warning: string_database parameter is not set in {self.config_file}. Will not add interaction information.")
 
     # TODO: Check whether these two logical conditions can be simplified
     if not self.hmmscan_database and self.pfam_database:
       self.hmmscan_database = self.species_prefix + "_hmmscan_database.txt"
       logging.info("Warning: hmmscan is being performed on all proteins. This can take a long time.")
     if not self.hmmscan_database and not self.pfam_database:
-      logging.info(f'Warning: pfam_database parameter is not set in {self.config_file}. Will not add domain information.')
+      logging.info(f"Warning: pfam_database parameter is not set in {self.config_file}. Will not add domain information.")
--- mipfinder/mipfinder.py
+++ mipfinder/mipfinder.py
@@ -5,17 +5,36 @@ WIP WIP WIP WIP
 
 from config import Config
 import logging
+import datetime
+import os
 
 # Set up logging configuration
 logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     filename = 'mipfinder.log',
                     filemode = 'w',
                     format='%(name)s:%(levelname)s:%(message)s')
 
-# TODO: Maybe rewrite using ConfigArgParser module rather than configuration file? Not important atm
-# Read configuration file (previously ArgParse CLI)
-# TODO: Change back to 'config.ini' in the final version. Can't get VS Code to run the script from the subdir
+if __name__ == "__main__":
+  logging.info("Starting miPFinder v2.0")
+  logging.info(f"Starting at {datetime.datetime.now()}")
+  logging.debug(f"Working directory is {os.getcwd()}")
 
-logging.info("Starting miPFinder v2.0")
-config = Config('./mipfinder/config.ini')
+  # TODO: Maybe rewrite using ConfigArgParser module rather than configuration file? Not important atm
+  # TODO: Change back to 'config.ini' in the final version. Can't get VS Code to run the script from the subdir
+  # Read configuration file (previously ArgParse CLI)
+  config = Config('./mipfinder/config.ini')
+
+
+# (Valdeko, 10/05/2019) TODO: Potentially move them into a configuration file and have three different variables? I don't know if
+# there variables are used for more than this or if this is it. 
+# blastPATH = '\"'+args['blastPATH']+'\"'
+# hmmsearchPATH = '\"'+args['hmmPATH']+'hmmsearch.exe\"'
+# hmmbuildPATH = '\"'+args['hmmPATH']+'hmmbuild.exe\"'
+# hmmscanPATH = '\"'+args['hmmPATH']+'hmmscan.exe\"'
+# clustalwPATH = '\"'+args['ClustalPATH']+'clustalw2.exe\"'
+
+
+# IDlist = args['IDlist'].split(';')
+
+# print '\nArguments:\n'+str(args).replace(',','\n')+'\nParsed arguments successfully, all tested dependencies are available'
