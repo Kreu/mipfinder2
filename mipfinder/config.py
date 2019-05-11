af4b8cd23ac426024bdf52381548022b76e87178
Kreu <kruusvee.valdeko@gmail.com>
2019-05-11 17:46:44 +0200

    Move verification code into config.py

--- mipfinder/config.py
+++ mipfinder/config.py
@@ -22,6 +22,7 @@ class Config:
     self.config_file = config_file
     self._readConfiguration()
     self._verifyConfiguration()
+    logging.info(f"{self.config_file} read successfully")
 
   def _readConfiguration(self):
     """Read the configuration file for miPFinder.
@@ -31,14 +32,13 @@ class Config:
     
     """
     if not os.path.exists(self.config_file):
-      raise FileNotFoundError(f"{config_file} could not be found, aborting...")
+        raise FileNotFoundError(f"{self.config_file} could not be found, aborting...")
     
-    logging.info(f"Reading configuration file {self.config_file}...")
+    logging.info(f"Reading configuration file ({self.config_file})...")
     config = configparser.ConfigParser()
     config.read(self.config_file)
     
     # MIPFINDER configuration section
-    self.protein_list : str = config['MIPFINDER']['protein_list']
     self.species_prefix : str = config['MIPFINDER']['species_prefix']
     self.maximum_mip_length : int = int(config['MIPFINDER']['maximum_mip_length'])
     self.minimum_ancestor_length : int = int(config['MIPFINDER']['minimum_ancestor_length'])
@@ -54,6 +54,7 @@ class Config:
     self.dropfile : str = config['MIPFINDER']['dropfile']
 
     # DATA configuration section
+    self.protein_list : str = config['DATA']['protein_list']
     self.hmmscan_database : str = config['DATA']['hmmscan_database']
     self.pfam_database : str = config['DATA']['pfam_database']
     self.ipfam_database : str = config['DATA']['ipfam_database']
@@ -75,10 +76,75 @@ class Config:
     """Verify that user configuration file has correct parameters.
     
     Raises:
-      ValueError: If, in configuration file `maximum_mip_length` is larger or equal to `minimum_ancestor_length`.
+      ValueError: If, in configuration file `maximum_mip_length` is larger or equal to 
+                  `minimum_ancestor_length`.
 
     """
     logging.info("Verifying configuration file...")
+
+    # TODO (11/05/2018, Valdeko): See which variables are optional and which are absolutely and do
+    # checking based on that
+    # ######ARGUMENT PARSER & VERIFICATION
+    # os.environ["CYGWIN"] = "nodosfilewarning" #avoid hmmer warnings
+    # currentPATH = os.getcwd().replace('\\','/')
+
+
+    # def isFile(string):
+        # if os.path.isfile(string) == False:
+    #         msg = "%r does not exist" % string
+    #         raise argparse.ArgumentTypeError(msg)
+    #     return string
+    # def isBlast(string):
+    # 	output = open('testtest.txt','w')
+    # 	output.write('>1\nCRTTATPMWRG\n>2\nCNTTKTPLWRS')
+    # 	output.close()
+    # 	try:
+    # 		subprocess.check_output(('\"'+string+'\"makeblastdb.exe -dbtype prot -in testtest.txt'), shell=True)
+    #   except subprocess.CalledProcessError, e:
+    # 		msg = "%r does not point to blast folder (tested for makeblastdb.exe)" % string
+    # 		subprocess.check_output(('del testtest.*'), shell=True)
+    # 		raise argparse.ArgumentTypeError(msg)
+    # 	return string
+    # def isClustal(string):
+    # 	output = open('testtest.txt','w')
+    # 	output.write('>1\nCRTTATPMWRG\n>2\nCNTTKTPLWRS')
+    # 	output.close()
+    # 	try:
+    # 		subprocess.check_output(('\"'+string+'\"clustalw2.exe -INFILE=testtest.txt -ALIGN -TYPE=PROTEIN -OUTFILE=testtest.txt'), shell=True)
+    # 	except subprocess.CalledProcessError, e:
+    # 		msg = "%r does not point to clustalw2 folder" % string
+    # 		subprocess.check_output(('del testtest.*'), shell=True)
+    # 		raise argparse.ArgumentTypeError(msg)
+    # 	return string
+    # def isHmmer(string):
+    # 	output = open('testtest.txt','w')
+    # 	output.write('1 MKVRSSVKKMCEFCKTVKRRGR\n2 MKIRASVRKICEKCRLIRRRGR')
+    # 	output.close()
+    # 	try:
+    # 		subprocess.check_output(('\"'+string+'hmmbuild.exe\" --amino testtest.txt '+currentPATH+'/testtest.txt'), shell=True)
+    # 	except subprocess.CalledProcessError, e:
+    # 		msg = "%r does not point to Hmmer folder (tested for hmmbuild.exe)"
+    # 		subprocess.check_output(('del testtest.*'), shell=True)
+    # 		raise argparse.ArgumentTypeError(msg)
+    # 	subprocess.check_output(('del testtest.*'), shell=True)
+    # 	return string
+    # def isInt(string):
+    # 	try:
+    # 		int(string)
+    # 	except:
+    # 		msg = "%r is not recognized as Integer, but must be!" % string
+    # 		raise argparse.ArgumentTypeError(msg)
+    # 	return int(string)
+    # def isFloat(string):
+    # 	try:
+    # 		float(string)
+    # 	except:
+    # 		msg = "%r is not recognized as Float, but must be!" % string
+    # 		raise argparse.ArgumentTypeError(msg)
+    # 	return float(string)
+
+
+
     if self.maximum_mip_length >= self.minimum_ancestor_length:
       logging.info(f"In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.")
       raise ValueError(f"In {self.config_file}, maximum_mip_length must be smaller than the minimum_ancestor_length.")
@@ -102,3 +168,15 @@ class Config:
 
     if not self.hmmscan_database and not self.pfam_database:
       logging.info(f"Warning: pfam_database parameter is not set in {self.config_file}. Will not add domain information.")
+
+
+# Helper functions for Config class
+def _fileExists(filename: str) -> bool:
+  """Check whether a file exists.
+
+  Returns:
+    bool: True is file exists, otherwise False.
+  
+  """
+  return os.path.exists(filename)
+
