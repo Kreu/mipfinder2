9e63498659e530759c6bf2ce710a4436ff437131
Kreu <kruusvee.valdeko@gmail.com>
2019-05-24 13:55:23 +0200

    Add tokenise function

--- mipfinder2/fasta.py
+++ mipfinder2/fasta.py
@@ -1,14 +1,13 @@
-import typing
 import logging
+from typing import Dict, List
 
-# TODO: Write unit tests for this
-def createFile(sequences: dict, output: str):
+def createFile(sequences: Dict[str, str], output_file: str):
   """Creates a FASTA file from the supplied protein IDs and sequences.
 
   Args:
-    sequences (dict): A dictionary containing protein IDs as keys and corresponding sequences as values
-    output (str): Name of the output file. Overwrites the file if it already exists, otherwise creates
-                  a new file.
+    sequences: A dictionary containing FASTA headers as keys and corresponding sequences as values
+    output_file: Name of the output file. Overwrites the file if it already exists, otherwise
+        creates a new file.
   
   """
   logging.info(f"Writing {output}.fasta")
@@ -18,15 +17,14 @@ def createFile(sequences: dict, output: str):
       f.write(f">{protein_id}\n{protein_sequence}\n")
   logging.info(f"Finished writing {output}.fasta")
 
-def extractRecords(fasta_file: str) -> typing.Dict[str, str]:
+def extractRecords(fasta_file: str) -> Dict[str, str]:
   """Extracts all FASTA records from a file into individual entries.
 
   Args:
-    fasta_file (str): Path to a file containing all the proteins of the target
-                      organism in FASTA format.
+    fasta_file: Path to the FASTA file.
+
   Returns:
-    Dictionary containing protein FASTA header as the key and protein sequence
-    as the value.
+    A dictionary containing a FASTA header as the key and the corresponding sequence as the value.
 
   """
   with open(fasta_file, 'r') as f:
@@ -72,16 +70,24 @@ def extractRecords(fasta_file: str) -> typing.Dict[str, str]:
 def getProteinExistenceLevel(fasta_header: str) -> int:
   """Extracts ProteinExistence level from a UniProtKB FASTA header.
 
+  From https://www.uniprot.org/help/fasta-headers:
+  * ProteinExistence is the numerical value describing the evidence for the existence of the protein. (From 1-5)
+    1. Experimental evidence at protein level
+    2. Experimental evidence at transcript level
+    3. Protein inferred from homology
+    4. Protein predicted
+    5. Protein uncertain
+
   Returns:
     An integer denoting the ProteinExistence level for a given header. If ProteinExistence
-    cannot be found within the header, return -1
+    cannot be found within the header, returns -1
 
   """
   # PE level is always a single digit number, we can just access the character after `PE=` substring.
   pe_position: int  = fasta_header.find("PE=")
   pe_level: int = -1
-  if pe_position != -1: #If string is not found, find() returns -1
-    pe_level = fasta_header[pe_position + 3] #It's `3` because in `PE=x` x denotes the PE level integer
+  if pe_position != -1:  #If string is not found, find() returns -1
+    pe_level = fasta_header[pe_position + 3]  # It's `3` because in `PE=x` x denotes the PE level integer
     return int(pe_level)
   else:
     return -1
@@ -89,41 +95,17 @@ def getProteinExistenceLevel(fasta_header: str) -> int:
 def extractUniprotID(fasta_header: str, protein_existence_cutoff: int) -> str:
   #TODO 15/05/2019, Valdeko: Maybe let user specify what record they want to extract,
   #possiby by specifying a column?
-  """Extract the Uniprot ID from a FASTA record.
+  """Extract the Uniprot ID from a UniProtKB FASTA header.
 
-  Different databases have different FASTA record headers, however MIPFINDER v2.0 has been developed
-  using UniProtKB database and serves as an example here.
-
-  A generic example of a FASTA record in UniprotKB is as follows:
-  >db|UniqueIdentifier|EntryName ProteinName OS=OrganismName OX=OrganismIdentifier [GN=GeneName ]PE=ProteinExistence SV=SequenceVersion
-
-  An example of an actual FASTA record header in UniProtKB is as follows.
-  >sp|Q9C5U0|AHK4_ARATH Histidine kinase 4 OS=Arabidopsis thaliana OX=3702 GN=AHK4 PE=1 SV=1
-
-  A detailed description of the header is found at https://www.uniprot.org/help/fasta-headers, 
-  here is just a copied version from their website:
-
-    * db is ‘sp’ for UniProtKB/Swiss-Prot and ‘tr’ for UniProtKB/TrEMBL.
-    * UniqueIdentifier is the primary accession number of the UniProtKB entry.
-    * EntryName is the entry name of the UniProtKB entry.
-    * ProteinName is the recommended name of the UniProtKB entry as annotated in the RecName field. For UniProtKB/TrEMBL entries without a RecName field, the SubName field is used. In case of multiple SubNames, the first one is used. The ‘precursor’ attribute is excluded, ‘Fragment’ is included with the name if applicable.
-    * OrganismName is the scientific name of the organism of the UniProtKB entry.
-    * OrganismIdentifier is the unique identifier of the source organism, assigned by the NCBI.
-    * GeneName is the first gene name of the UniProtKB entry. If there is no gene name, OrderedLocusName or ORFname, the GN field is not listed.
-    * ProteinExistence is the numerical value describing the evidence for the existence of the protein. (From 1-5)
-        1. Experimental evidence at protein level
-        2. Experimental evidence at transcript level
-        3. Protein inferred from homology
-        4. Protein predicted
-        5. Protein uncertain
-    * SequenceVersion is the version number of the sequence.
+  For a detailed description of UniProt FASTA headers, see here:
+  https://www.uniprot.org/help/fasta-headers
 
   Args:
-    fasta_header (str): String representation of the FASTA header for a protein record
-    protein_existence_cutoff (int): An integer representing which ProteinExistence levels (see
-                                    above) to filter out. Filters out everything that is equal to 
-                                    AND larger (e.g. protein_existence_cutoff=3 only shows proteins
-                                    with PE level of 1 and 2)
+    fasta_header: UniProt FASTA header for a protein record.
+    protein_existence_cutoff: An integer representing which ProteinExistence levels (see
+        above) to filter out. Filters out everything that is equal to 
+        AND larger (e.g. protein_existence_cutoff=3 only shows proteins
+        with PE level of 1 and 2)
 
   Returns:
     A string containing the UniProt ID of the given FASTA record. If no UniProt ID can be detected 
@@ -138,3 +120,30 @@ def extractUniprotID(fasta_header: str, protein_existence_cutoff: int) -> str:
   split_header: str = fasta_header.split('|')
   uniprot_id: str = split_header[1]
   return uniprot_id
+
+
+def tokenise(string: str, delimiter: str) -> List[str]:
+  """Splits a string into tokens based on the specified delimiter(s).
+
+  Args:
+    string: String to be tokenised.
+    delimiter: Regular expression containing the delimiters to split the string by. 
+
+  Returns:
+    A list of tokens in a string format, split at the specified delimiter(s).
+
+  Example:
+    Split a string at every comma (,) and hyphen (-):
+      >>> string = "Test, string to-be split"
+      >>> tokenise(string, "[,\-]+")
+      ["Test", " string to", "be split"]
+
+    Split a string at every space and exclamation point:
+      >>> string = "This! Is another example!"
+      >>> tokenise(string, "[ !]+")
+      ["This", "Is", "another", "exmple"]
+
+  """
+
+  tokens = filter(None, re.split(delimiter, string))
+  return tokens
